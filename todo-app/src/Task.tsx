2dbfcc79f410e0a333a75e027265fb5f9919b0d7
Marcin Grygierek <grygierek.marcin@gmail.com>
2021-07-05 15:40:15 +0200

    time difference

--- todo-app/src/Task.tsx
+++ todo-app/src/Task.tsx
@@ -1,3 +1,4 @@
+import { differenceInSeconds } from "date-fns";
 import { SingleTask } from "./types/task";
 
 interface Props {
@@ -7,19 +8,29 @@ interface Props {
 }
 
 const Task = ({task, finishTask, reopenTask}: Props) => {
-  return (
-    <li className="card mt-3">
-        <div className="card-body">
-            <h2 className="cart-title">{task.name}</h2>
-            <p>Created at: {task.createdAt.toLocaleString()}</p>
-            {task.finishedAt && <p>Finished at: {task.finishedAt.toLocaleString()}</p>}
 
-            {task.finishedAt 
-            ? <button className='btn btn-lg btn-secondary' onClick={() => reopenTask(task.id)}>Reopen</button>  
-            : <button className='btn btn-lg btn-success' onClick={() => finishTask(task.id)}>Close</button> }
-        </div>
-    </li>
-  )
+    const calculateDifference = () =>  {
+        if(!task.finishedAt) return null;
+        return differenceInSeconds(task.finishedAt, task.createdAt);
+    }
+    
+
+    return (
+        <li className="card mt-3">
+            <div className="card-body">
+                <h2 className="cart-title">{task.name}</h2>
+                <p>Created at: {task.createdAt.toLocaleString()}</p>
+                {task.finishedAt && <>
+                    <p>Finished at: {task.finishedAt.toLocaleString()}</p>
+                    <p>Elapsed time <div className="badge bg-primary">{calculateDifference()} s</div></p>
+                </>}
+
+                {task.finishedAt 
+                ? <button className='btn btn-lg btn-secondary' onClick={() => reopenTask(task.id)}>Reopen</button>  
+                : <button className='btn btn-lg btn-success' onClick={() => finishTask(task.id)}>Close</button> }
+            </div>
+        </li>
+    )
 }
 {/* 
         <div className="card mt-3">
