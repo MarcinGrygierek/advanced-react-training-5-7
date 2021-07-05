efaba7e353464bfd22356f5c5ff60b2b06857bec
Marcin Grygierek <grygierek.marcin@gmail.com>
2021-07-05 14:05:23 +0200

    fixed name

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
                 Create new task
               </h1>
               <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <input type="text" className="form-control" placeholder="Task name" aria-label="Task name" aria-describedby="button-addon2" />
                 <button className="btn btn-primary" type="button" id="button-addon2">Button</button>
               </div>
             </div>
