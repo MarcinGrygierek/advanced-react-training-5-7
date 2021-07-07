82fb13598f5bb2ff1863ea141b081b08c5bc2d6c
Marcin Grygierek <grygierek.marcin@gmail.com>
2021-07-07 14:11:27 +0200

    react routing

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,12 +1,27 @@
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Link, Switch, Route, Redirect, useHistory} from 'react-router-dom';
 import { AppContainer } from './AppContainer';
 import { Form } from './Form';
 import { useTasks } from './hooks/useTasks';
 import store from './store';
 import { Tasks } from './Tasks';
+import { SingleTask } from './types/task';
 
 const TaskManager = () => {
   const {tasks, reopenTask, finishTask, addNewTask} = useTasks();
+  const history = useHistory();
+
+  useEffect(() => {
+    const closed = tasks.filter((task: SingleTask) => task.finishedAt);
+    history.block(() => {
+     if(closed.length < 5) {
+       console.log('TODO: display modal with warning');
+       return false;
+     }
+    })
+  }, [tasks])
+
   return (
         <AppContainer>
           <Form addNewTask={addNewTask} />
@@ -15,10 +30,34 @@ const TaskManager = () => {
   );
 }
 
+const About = () => {
+  return <h1>This is my super task manager.</h1>
+}
+
 const App = () => {
   return (
     <Provider store={store}>
-       <TaskManager />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/tasks'>Tasks</Link>
+            </li>
+
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to='/tasks' />} />
+          <Route path='/about' component={About} />
+          <Route path='/tasks'>
+            <TaskManager />
+          </Route>
+        </Switch>
+       
+      </Router>
     </Provider>
   );
 }
